a291111427d24fdd13706d845ad130db4b621663
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-12-08 15:50:25 +0300

    initial commit

---
+++ markdown2html.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script checks if file exists."""
+
+import sys
+import os
+
+if len(sys.argv) != 3:
+  print("Usage: ./markdown2html.py README.md README.html")
+
+if os.path.exists("file.txt") == False:
+  print("Missing {}".format(sys.argv[1]))
+  exit 1
+  
+ else:
+  print()
+  exit 0
