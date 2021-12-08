79080645f2834a9d857a69a8474cc03952143a7b
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-12-08 16:21:35 +0300

    better logic

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,13 @@
-#!/usr/bin/python3
-"""This script checks if file exists."""
-
 import sys
 import os
 
 if len(sys.argv) != 3:
     print("Usage: ./markdown2html.py README.md README.html")
-
+    exit(1)
 if os.path.exists(sys.argv[1]) == False:
     print("Missing {}".format(sys.argv[1]))
-    exit (1)
+    exit(1)
   
 else:
     print()
-    exit (0)
+    exit(0)
