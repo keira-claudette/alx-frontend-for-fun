539167f71c0b01a346d3c6e2cb018063e1d07cb0
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-12-08 16:25:02 +0300

    added if __name__ == "__main__":

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,15 @@
-import sys
-import os
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import os
 
-if len(sys.argv) != 3:
-    print("Usage: ./markdown2html.py README.md README.html")
-    exit(1)
-if os.path.exists(sys.argv[1]) == False:
-    print("Missing {}".format(sys.argv[1]))
-    exit(1)
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+    if os.path.exists(sys.argv[1]) == False:
+        print("Missing {}".format(sys.argv[1]))
+        exit(1)
   
-else:
-    print()
-    exit(0)
+    else:
+        print()
+        exit(0)
