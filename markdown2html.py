06330147556bf754b266c8d0f77aa245c10b6b0c
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-12-13 21:12:12 +0300

    printed to stderror

--- markdown2html.py
+++ markdown2html.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     import sys
     import os
 
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     if os.path.exists(sys.argv[1]) == False:
-        print("Missing {}".format(sys.argv[1]))
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         exit(1)
   
     else:
