02d75c83d215316002cb3473a432e82ab61d2357
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-12-13 21:17:59 +0300

    added <ul> translation

--- markdown2html.py
+++ markdown2html.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ This module translates markdown to html."""
+
 if __name__ == "__main__":
     import sys
     import os
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     if os.path.exists(sys.argv[1]) == False:
@@ -12,5 +13,18 @@ if __name__ == "__main__":
         exit(1)
   
     else:
-        print()
+ 
+        template = {"1": "<h1>", "2": "<h2>", "3": "<h3>", "4": "<h4>", "5": "<h5>", "6": "<h6>", "-": "<li>"}
+        with open(sys.argv[1], 'r') as f:
+            for line in f:
+                with open(sys.argv[2], 'w') as html_file:
+                    count = line.count("#")
+                    count = str(count)
+                    for k, v in template.items():
+                        if count == k:                            
+                            l = v + line.strip("#, \n") + "</" + v.strip("<") + "\n"
+                            html_file.write(l)
+                        if line[0] == k:
+                            l = "<ul>\n" + v + line[1:-1] +  v.replace("<", "</") + "\n</ul>"
+                            html_file.write(l)
         exit(0)
